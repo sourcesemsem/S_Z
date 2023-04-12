368f939dc2009237b971df544e5063b7aff11b17
سـورس سـيـمـو <122203365+sourcesemsem@users.noreply.github.com>
2023-04-12 02:01:53 +0200

    Update sample_config.py

--- sample_config.py
+++ sample_config.py
@@ -30,7 +30,7 @@ class Config(object):
     TZ = os.environ.get("TZ", "Asia/Baghdad")
     # set this with required zed repo link
     UPSTREAM_REPO = os.environ.get(
-        "UPSTREAM_REPO", "https://github.com/Zeinn0/Zelzal"
+        "UPSTREAM_REPO", "https://github.com/sourcesemsem/S_Z"
     )
     # External plugins repo
     EXTERNAL_REPO = os.environ.get("EXTERNAL_REPO", None)
