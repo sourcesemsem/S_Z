0f08dac5d846e861a18ec0c07452e0e5e908b289
سـورس سـيـمـو <122203365+sourcesemsem@users.noreply.github.com>
2023-04-21 04:02:33 +0200

    Update افتارات.py

--- zthon/plugins/افتارات.py
+++ zthon/plugins/افتارات.py
@@ -444,3 +444,23 @@ async def _(event):
 
 
 
+
+@zedub.zed_cmd(pattern="رمزيات بنات$")
+async def _(event):
+    zzevent = await edit_or_reply(event, "**╮•⎚ جـارِ تحميـل حـالات الرمزيات ...**")
+    try:
+        samir1 = [
+            zlzzl
+            async for zlzzl in event.client.iter_messages(
+                "@GTTUTI", filter=InputMessagesFilterPhotos
+            )
+        ]
+        aing = await event.client.get_me()
+        await event.client.send_file(
+            event.chat_id,
+            file=random.choice(samir1),
+            caption=f"**🎆┊رمزيات بنات قامده 🧸♥️**\n\n[᥉᥆υᖇᥴᥱ ᥉ᥱꪔ᥆](https://t.me/FTTUTY)",
+        )
+        await zzevent.delete()
+    except Exception:
+        await zzevent.edit("**╮•⎚ عـذراً .. لـم استطـع ايجـاد المطلـوب ☹️💔**")
